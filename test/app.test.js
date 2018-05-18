174fe491a597a13491af4a13ca4a67d70a3110f4
william <willshinji@gmail.com>
2018-05-18 13:01:15 -0300

    creates cars for testing

--- test/app.test.js
+++ test/app.test.js
@@ -6,14 +6,32 @@ const expect = chai.expect;
 
 const chaiHttp = require('chai-http');
 
+const Car = require('../models/car');
+
 
 chai.use(chaiHttp);
 
 describe('API /cars', () => {
 
-  describe('GET /cars', () => {
+  let car;
 
-    it('returns status code 200', (done) => {
+  describe('GET /cars', () => {
+    before((done) =>{
+      // console.log('creating car for tests')
+      car = Car.create( { model: 'Ferrari 458 Italia', year: 2017, category: 'Sportive' } )
+      done();
+    });
+
+    after((done) => {
+      Car.remove({}, error => {
+        if (error)
+            console.log('error')
+
+      });
+      done();
+    });
+
+    it('returns status code 200', done => {
       chai.request(app).get('/cars')
         .end( (error, response) => {
           expect(response.status).to.be.equal(200);
@@ -21,13 +39,26 @@ describe('API /cars', () => {
         });
     });
 
-    it('returns response ok true', (done) => {
+    it('returns response ok true', done => {
       chai.request(app).get('/cars')
         .end( (error, response) =>{
            expect(response.ok).to.be.true;
            done();
         });
     });
+
+    it('returns car', done => {
+      chai.request(app).get('/cars')
+        .end((error, response) => {
+          // console.log(response.body)
+          // console.log(response.body.results[0].model)
+          expect(response.body.results[0].model).to.be.equal('FERRARI 458 ITALIA');
+          expect(response.body.results[0].category).to.be.equal('Sportive');
+
+          done();
+        });
+    });
+
   });
 
 })
