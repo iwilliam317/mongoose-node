a5d01a862bed9bf239e3f02538739ebad9446382
william <willshinji@gmail.com>
2018-05-28 13:30:34 -0300

    adds test context

--- test/app.test.js
+++ test/app.test.js
@@ -11,85 +11,88 @@ const Car = require('../models/car');
 
 chai.use(chaiHttp);
 
-describe('API /cars', () => {
+describe('Routes', () => {
 
-  let car;
+  describe('API /cars', () => {
 
-  before( done =>{
-    // console.log('creating car for tests')
-    car = Car.create( { model: 'Ferrari 458 Italia', year: 2017, category: 'Sportive' } )
-    done();
-  });
+    let car;
 
-  after( done => {
-    Car.remove({}, error => {
-       // console.log('cleans car's collection')
-      if (error)
-          console.log('error')
-    });
-      
+    before( done =>{
+      // console.log('creating car for tests')
+      car = Car.create( { model: 'Ferrari 458 Italia', year: 2017, category: 'Sportive' } )
       done();
     });
 
-  describe('GET /cars', () => {
+    after( done => {
+      Car.remove({}, error => {
+         // console.log('cleans car's collection')
+        if (error)
+            console.log('error')
+      });
+        
+        done();
+      });
 
-    it('returns status code 200', done => {
-      chai.request(app).get('/cars')
-        .end( (error, response) => {
-          expect(response.status).to.be.equal(200);
-         done();
-        });
-    });
+    describe('GET /cars', () => {
 
-    it('returns response ok true', done => {
-      chai.request(app).get('/cars')
-        .end( (error, response) =>{
-           expect(response.ok).to.be.true;
+      it('returns status code 200', done => {
+        chai.request(app).get('/cars')
+          .end( (error, response) => {
+            expect(response.status).to.be.equal(200);
            done();
-        });
-    });
+          });
+      });
 
-    it('returns car', done => {
-      chai.request(app).get('/cars')
-        .end((error, response) => {
-          // console.log(response.body)
-          // console.log(response.body.results[0].model)
-          expect(response.body.results[0].model).to.be.equal('FERRARI 458 ITALIA');
-          expect(response.body.results[0].category).to.be.equal('Sportive');
-          done();
-        });
-    });
-
-  });
-
-  describe('GET /cars/:id',  () =>{
-    // working on it
-    xit('asdasd', async (done) =>{
-      
-      try{
-        const currentCar = await Car.findOne({ model: 'FERRARI 458 ITALIA'});
-          console.log(currentCar._id)
-          const {_id } = result;
-      
-
-        chai.request(app).get(`/cars/${_id}`)
-          .end((error, response) => {
-            console.log(response.body.results.model)
-            // expect(response.body.results.model).to.be.equal('FERRARI 458 ITALIA');
+      it('returns response ok true', done => {
+        chai.request(app).get('/cars')
+          .end( (error, response) =>{
+             expect(response.ok).to.be.true;
              done();
           });
+      });
 
-      }
-      catch{
-        
-      }
-      // console.log(currentCar);
-      // console.log(`/cars/${_id}`);
-
-     
+      it('returns car', done => {
+        chai.request(app).get('/cars')
+          .end((error, response) => {
+            // console.log(response.body)
+            // console.log(response.body.results[0].model)
+            expect(response.body.results[0].model).to.be.equal('FERRARI 458 ITALIA');
+            expect(response.body.results[0].category).to.be.equal('Sportive');
+            done();
+          });
+      });
 
     });
-    // const id = currentCar._id;
-  });
 
-})
+    describe('GET /cars/:id',  () =>{
+      // working on it
+      xit('asdasd', async (done) =>{
+        
+        try{
+          const currentCar = await Car.findOne({ model: 'FERRARI 458 ITALIA'});
+            console.log(currentCar._id)
+            const {_id } = result;
+        
+
+          chai.request(app).get(`/cars/${_id}`)
+            .end((error, response) => {
+              console.log(response.body.results.model)
+              // expect(response.body.results.model).to.be.equal('FERRARI 458 ITALIA');
+               done();
+            });
+
+        }
+        catch{
+          
+        }
+        // console.log(currentCar);
+        // console.log(`/cars/${_id}`);
+
+       
+
+      });
+      // const id = currentCar._id;
+    });
+
+  })
+});
